74596c10e782904eec77ff329ff85e7a7df17955
eraxxon <eraxxon@localhost>
2003-09-18 00:44:30 +0000

    [project @ 2003-09-17 19:44:30 by eraxxon] Create Ctxt stack, etc.

--- src/xaif2whirl/XlationContext.cxx
+++ src/xaif2whirl/XlationContext.cxx
@@ -1,5 +1,5 @@
 // -*-Mode: C++;-*-
-// $Header: /Volumes/cvsrep/developer/OpenADFortTk/src/xaif2whirl/XlationContext.cxx,v 1.2 2003/09/02 15:02:21 eraxxon Exp $
+// $Header: /Volumes/cvsrep/developer/OpenADFortTk/src/xaif2whirl/XlationContext.cxx,v 1.3 2003/09/17 19:44:30 eraxxon Exp $
 
 // * BeginCopyright *********************************************************
 // *********************************************************** EndCopyright *
@@ -39,11 +39,66 @@ XlationContext::XlationContext()
   wn2idMap = NULL;
   id2wnMap = NULL;
   xsym2wsymMap = NULL;
+
+  ctxtstack.push_front(Ctxt());
 }
 
 XlationContext::~XlationContext()
 {
+  ctxtstack.clear(); // clear the stack
 }
+
+
+// -------------------------------------------------------
+// Context manipulation (Create, Delete...)
+// -------------------------------------------------------
+
+XlationContext&
+XlationContext::CreateContext()
+{
+  return Ctor(NOFLAG);
+}
+
+XlationContext&
+XlationContext::CreateContext(uint32_t flags_)
+{
+  return Ctor(flags_);
+}
+
+XlationContext&
+XlationContext::Ctor(uint32_t flags_)
+{
+  // If available, get enclosing context
+  const Ctxt* enclCtxt = NULL;
+  if (ctxtstack.size() > 0) {
+    enclCtxt = &(ctxtstack.front());
+  }
+  
+  // Create new context
+  ctxtstack.push_front(Ctxt());
+  CurContext().SetFlags(flags_);
+
+  // Set inherited flags from enclosing context
+  Ctxt& curCtxt = CurContext();
+  if (enclCtxt->AreFlags(VARREF)) { curCtxt.SetFlags(VARREF); }
+  if (enclCtxt->AreFlags(LVALUE)) { curCtxt.SetFlags(LVALUE); }
+  
+  return (*this);
+}
+
+XlationContext&
+XlationContext::DeleteContext()
+{
+  if (ctxtstack.size() > 1) {
+    // maintain invariant that there is at least one context
+    ctxtstack.pop_front();
+  }
+  return (*this);
+}
+
+// -------------------------------------------------------
+// Id maps
+// -------------------------------------------------------
 
 pair<ST_TAB*, PU_Info*>
 XlationContext::FindSymTab(SymTabId stabId)
@@ -70,10 +125,10 @@ XlationContext::FindWNId(WN* wn)
 }
 
 WN*
-XlationContext::FindWN(WNId wnid)
-  {
+XlationContext::FindWN(WNId wnid, bool mustFind)
+{
   WNIdToWNMap* map = GetIdToWNMap();
-  if (map) { return (map->Find(wnid)); }
+  if (map) { return (map->Find(wnid, mustFind)); }
   return NULL;
 }
 
@@ -85,6 +140,9 @@ XlationContext::FindSym(const char* scopeid, const char* symid)
   return NULL;
 }
 
+// -------------------------------------------------------
+// Misc
+// -------------------------------------------------------
 
 void 
 XlationContext::Dump(std::ostream& o, const char* pre) const
@@ -97,3 +155,27 @@ XlationContext::DDump() const
   Dump(std::cerr);
 }
 
+//***************************************************************************
+// XlationContext::Ctxt
+//***************************************************************************
+
+XlationContext::Ctxt::Ctxt()
+{
+}
+
+XlationContext::Ctxt::~Ctxt()
+{
+}
+
+void 
+XlationContext::Ctxt::Dump(std::ostream& o) const
+{
+  o << "{context}";
+  CtxtFlags::Dump(o);
+}
+
+void 
+XlationContext::Ctxt::DDump() const
+{
+  Dump(std::cerr);
+}
--- src/xaif2whirl/XlationContext.h
+++ src/xaif2whirl/XlationContext.h
@@ -1,5 +1,5 @@
 // -*-Mode: C++;-*-
-// $Header: /Volumes/cvsrep/developer/OpenADFortTk/src/xaif2whirl/XlationContext.h,v 1.2 2003/09/02 15:02:21 eraxxon Exp $
+// $Header: /Volumes/cvsrep/developer/OpenADFortTk/src/xaif2whirl/XlationContext.h,v 1.3 2003/09/17 19:44:30 eraxxon Exp $
 
 // * BeginCopyright *********************************************************
 // *********************************************************** EndCopyright *
@@ -28,6 +28,7 @@
 
 //*************************** User Include Files ****************************
 
+#include <lib/support/XlationCtxt.h>
 #include <lib/support/WhirlIDMaps.h>
 #include <lib/support/SymTab.h>
 
@@ -41,10 +42,62 @@
 
 class XlationContext
 {
+public: 
+  
+  // Ctxt: represents the current context
+  class Ctxt : public CtxtFlags {
+  public: 
+    Ctxt();
+    virtual ~Ctxt();
+
+    virtual void Dump(std::ostream& o = std::cerr) const;
+    virtual void DDump() const;
+    
+  private:
+  };
+
+  enum Flags {
+    NOFLAG           = 0x00000000,
+    VARREF           = 0x00000001, // within variable reference; inherited
+    LVALUE           = 0x00000002, // var ref should be an lvalue (inherited)
+  };
+
 public:
   XlationContext();
   virtual ~XlationContext();
   
+  // -------------------------------------------------------
+  // Context manipulation (Create, Delete...)
+  // -------------------------------------------------------
+
+  // Create a new child context and make it the current context.  One
+  // can pass flags that should apply to the new context.  Note that
+  // that these flags *do not* override any inherited flags.
+  XlationContext& CreateContext();
+  XlationContext& CreateContext(uint32_t flags_);
+
+  // Delete the current context and make its parent the current
+  // context.  Everything (including memory) associated with the old
+  // context will be deleted.
+  XlationContext& DeleteContext();
+
+  // Returns the current context
+  Ctxt& CurContext() const { return const_cast<Ctxt&>(ctxtstack.front()); }
+
+  // -------------------------------------------------------
+  // Flags
+  // -------------------------------------------------------
+  
+  // Within a variable reference (inherited)
+  BOOL IsVarRef() const { return CurContext().AreFlags(VARREF); }
+  void SetVarRef()      { CurContext().SetFlags(VARREF); }
+  void ResetVarRef()    { CurContext().ResetFlags(VARREF); }
+
+  // variable reference should be an lvalue (inherited)
+  BOOL IsLValue() const { return CurContext().AreFlags(LVALUE); }
+  void SetLValue()      { CurContext().SetFlags(LVALUE); }
+  void ResetLValue()    { CurContext().ResetFlags(LVALUE); }
+
   // -------------------------------------------------------
   // Id maps
   // -------------------------------------------------------
@@ -65,7 +118,7 @@ public:
   void SetWNToIdMap(WNToWNIdMap* x) { wn2idMap = x; }
   
   // WNId -> WN* map: (We do not assume ownership of the map)
-  WN* FindWN(WNId wnid);
+  WN* FindWN(WNId wnid, bool mustFind = false);
   WNIdToWNMap* GetIdToWNMap() const { return id2wnMap; }
   void SetIdToWNMap(WNIdToWNMap* x) { id2wnMap = x; }
   
@@ -74,21 +127,37 @@ public:
   XAIFSymToWhirlSymMap* GetXAIFSymToWhirlSymMap() const { return xsym2wsymMap; }
   void SetXAIFSymToWhirlSymMap(XAIFSymToWhirlSymMap* x) { xsym2wsymMap = x; }
   
+  // -------------------------------------------------------
+  // Misc
+  // -------------------------------------------------------
   
   virtual void Dump(std::ostream& o = std::cerr, const char* pre = "") const;
   virtual void DDump() const;
   
+
 private: 
   // Disable for now
   XlationContext(const XlationContext& x) { }
   XlationContext& operator=(const XlationContext& x) { return *this; }
   
+  XlationContext& Ctor(uint32_t flags_);
+  
+  // Use a list instead a stack so that we can easily examine
+  // contents.  The top of the stack will be the *front* of the
+  // list. (push_front, pop_front).
+  typedef std::list<Ctxt>           CtxtStack;
+  typedef CtxtStack::iterator       CtxtStackIt;
+  typedef CtxtStack::const_iterator CtxtStackItC;
+
+
 private:
   SymTabIdToSymTabMap* id2stabMap;
   PUIdToPUMap* id2puMap;
   WNToWNIdMap* wn2idMap;
   WNIdToWNMap* id2wnMap;
   XAIFSymToWhirlSymMap* xsym2wsymMap;
+
+  CtxtStack ctxtstack;
 };
 
 #endif /* XlationContext_INCLUDED */
