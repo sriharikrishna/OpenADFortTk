b0f6cae7e0ebc2e0327635629214a1a710331fcc
eraxxon <eraxxon@localhost>
2004-08-23 23:44:42 +0000

    [project @ 2004-08-23 18:44:42 by eraxxon] minor changes

--- src/whirl2sexp/main.cxx
+++ src/whirl2sexp/main.cxx
@@ -1,39 +1,5 @@
 // -*-Mode: C++;-*-
-// $Header: /Volumes/cvsrep/developer/OpenADFortTk/src/whirl2sexp/main.cxx,v 1.1 2004/08/05 18:38:14 eraxxon Exp $
-
-// * BeginCopyright *********************************************************
-/*
-  Copyright (C) 2000, 2001 Silicon Graphics, Inc.  All Rights Reserved.
-
-  This program is free software; you can redistribute it and/or modify it
-  under the terms of version 2 of the GNU General Public License as
-  published by the Free Software Foundation.
-
-  This program is distributed in the hope that it would be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-
-  Further, this software is distributed without any warranty that it is
-  free of the rightful claim of any third person regarding infringement 
-  or the like.  Any license provided herein, whether implied or 
-  otherwise, applies only to this software file.  Patent licenses, if 
-  any, provided herein do not apply to combinations of this program with 
-  other software, or any other product whatsoever.  
-
-  You should have received a copy of the GNU General Public License along
-  with this program; if not, write the Free Software Foundation, Inc., 59
-  Temple Place - Suite 330, Boston MA 02111-1307, USA.
-
-  Contact information:  Silicon Graphics, Inc., 1600 Amphitheatre Pky,
-  Mountain View, CA 94043, or:
-
-  http://www.sgi.com
-
-  For further information regarding this notice, see:
-
-  http://oss.sgi.com/projects/GenInfo/NoticeExplan
-*/
-// *********************************************************** EndCopyright *
+// $Header: /Volumes/cvsrep/developer/OpenADFortTk/src/whirl2sexp/main.cxx,v 1.2 2004/08/23 18:44:42 eraxxon Exp $
 
 //***************************************************************************
 //
@@ -45,8 +11,6 @@
 //
 // Description:
 //   [The set of functions, macros, etc. defined in the file]
-//
-// Based on Open64 be/be/driver.cxx (curr.driver.cxx)
 //
 //***************************************************************************
 
@@ -157,20 +121,14 @@ real_main(int argc, char **argv)
   FortTk_SetDiagnosticFilterLevel(args.debug);
   
   // -------------------------------------------------------
-  // 3. Read WHIRL IR
+  // 3. Read WHIRL and Translate into S-expressions
   // -------------------------------------------------------
   PU_Info* pu_forest = ReadIR(args.whirlFileNm.c_str());
-  PrepareIR(pu_forest); // FIXME (should this be part of translation?)
-
-  // -------------------------------------------------------
-  // 4. Translate WHIRL into S-expressions
-  // -------------------------------------------------------  
   whirl2sexp::TranslateIR(*os, pu_forest);
-  
   FreeIR(pu_forest); // Writing frees some of the WHIRL maps
   
   // -------------------------------------------------------
-  // 5. Finalization
+  // 4. Finalization
   // -------------------------------------------------------
   FiniOutputStream(os);
   
