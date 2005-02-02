5983e016b44594882dedfe8eacf10a9d1afb6e3d
eraxxon <eraxxon@localhost>
2005-02-02 03:52:24 +0000

    [project @ 2005-02-01 21:52:24 by eraxxon] initial checkin

---
+++ src/lib/support/sexputil.cxx
@@ -0,0 +1,31 @@
+// -*-Mode: C++;-*-
+// $Header: /Volumes/cvsrep/developer/OpenADFortTk/src/lib/support/sexputil.cxx,v 1.1 2005/02/01 21:52:24 eraxxon Exp $
+
+//***************************************************************************
+//
+// File:
+//   $Source: /Volumes/cvsrep/developer/OpenADFortTk/src/lib/support/sexputil.cxx,v $
+//
+// Purpose:
+//   Utilities for use with the sexp library.  Essentially, a set of
+//   wrappers for accessing data.
+//
+// Description:
+//   [The set of functions, macros, etc. defined in the file]
+//
+//***************************************************************************
+
+//************************** System Include Files ***************************
+
+//*************************** Sexp Include Files ****************************
+
+//*************************** User Include Files ****************************
+
+#include "sexputil.h"
+
+//************************** Forward Declarations ***************************
+
+//***************************************************************************
+
+//***************************************************************************
+
