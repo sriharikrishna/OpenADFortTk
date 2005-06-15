ee1dc067af33fef12f7fd5be31a820897a88f550
eraxxon <eraxxon@localhost>
2005-06-15 20:43:39 +0000

    [project @ 2005-06-15 15:43:39 by eraxxon] Fix file renaming to support .f90 extensions

--- tools/multiprocess/pre.pl
+++ tools/multiprocess/pre.pl
@@ -11,7 +11,7 @@ use Active_Chg;
 use File::Basename;
 
 my($infile) = $ARGV[0];
-my($name,$dir,$ext) = fileparse($infile,'\.[Ff]');
+my($name,$dir,$ext) = fileparse($infile,'\.[Ff][0-9]*');
 my($outfile) = $dir . $name . ".pre" . $ext;
 
 Ffile->new($infile)->rewrite_sem(\&active_chg)->write($outfile);
