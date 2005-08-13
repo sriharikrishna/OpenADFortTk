412e02bb62fafdd02be5d4ac414f8bcbb94d28b0
utke <utke@localhost>
2005-08-13 00:25:36 +0000

    [project @ 2005-08-12 19:25:36 by utke] added some intrinsics

--- tools/multiprocess/Lib/Intrinsic.pm
+++ tools/multiprocess/Lib/Intrinsic.pm
@@ -1,12 +1,24 @@
 package Intrinsic;
 
 @table = qw (
+	     sqrt
 	     exp
 	     sin
 	     cos
 	     tan
+	     tanh
+	     atan
 	     atan2
+	     mod
+	     abs
+	     sign
+	     int
+	     nint
+	     float
+	     open
+	     read
 	     write
+	     close
          );
 
 %table = map { $_=> 1 } @table;
