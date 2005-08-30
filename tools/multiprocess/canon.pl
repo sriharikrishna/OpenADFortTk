d404fb19185ae34b7d8558fdf6eee3004b2f23a3
utke <utke@localhost>
2005-08-30 01:21:18 +0000

    [project @ 2005-08-29 20:21:18 by utke] change to specify input and output files on the command line

--- tools/multiprocess/canon.pl
+++ tools/multiprocess/canon.pl
@@ -5,18 +5,16 @@ use lib "$RealBin/Lib/FT";
 use FTcontext;
 use Canonicalizer;
 
-die "You must specify exactly 1 file on the command line\n"
-    unless (@ARGV == 1);
+die "You must specify exactly 2 files (input, output) on the command line\n"
+    unless (@ARGV == 2);
 
-print STDERR "canonicalizing $ARGV[0], creating $ARGV[0].bak\n";
+print STDERR "canonicalizing $ARGV[0], creating $ARGV[1]\n";
 
-rename($ARGV[0],"$ARGV[0].bak");
-
-$ft = FTcontext->file("$ARGV[0].bak");
+$ft = FTcontext->file("$ARGV[0]");
 
 Canonicalizer::__set_verbose(1);
 
-print STDERR "File $ARGV[0], read in (and backed up)\nCommencing canonicalization...\n";
+print STDERR "File $ARGV[0] read; \nCommencing canonicalization...\n";
 $ft->crewrite_sem($fn2sub)
     ->crewrite_sem($add_fun_repl_decls)
-    ->write($ARGV[0]);
+    ->write($ARGV[1]);
