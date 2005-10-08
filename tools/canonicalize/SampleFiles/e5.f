0b75b104b36a4df7a71aca6a6d530acfe2f5a79b
mfagan <mfagan@localhost>
2005-10-08 01:38:18 +0000

    [project @ 2005-10-07 20:38:17 by mfagan] additional test files to reflect my acceptance test codes

---
+++ tools/canonicalize/SampleFiles/e5.f
@@ -0,0 +1,20 @@
+c This is a comment
+c
+
+	module foo
+
+	integer kkk
+	real(w2f_kind)  jjj
+
+	end module foo
+
+	subroutine ggg(xx,yy)
+
+	double precision :: x(15),y
+
+	y = gigig(x(3),hh(x(1),x(2)))
+
+	end
+
+
+
--- tools/canonicalize/SampleFiles/f1.f
+++ tools/canonicalize/SampleFiles/f1.f
@@ -1,16 +1,19 @@
-      subroutine foo(x)
-      x = x +
-c
-c embedded continuation lines
-c
-     &5 + 2 * x
-c
-c more embedded lines
-c
-     & + si
-     &n(x+2.0)
-      x = 5.0
-      x
-     & = 
-     & 13.2
-      end
+C
+C     **** Statements ****
+C
+      t__922 = IMAX
+      t__924 = MAX(t__922, 0_w2f__i8)
+      JWTYPE = 2
+      doloop_ub = IMAX
+      DO I = 1, doloop_ub, 1
+        FACZ = SWDK(I) * FACT
+        IF(FACZ .LT.(-2.0D+02)) THEN
+          SWDK(I) = 0.0
+        ELSE
+          SWDK(I) = (RFAC(JWTYPE) * EXP(FACZ / A1(JWTYPE)) + EXP(FACZ /
+     >  A2(JWTYPE)) *(1.0D00 - RFAC(JWTYPE)))
+        ENDIF
+      END DO
+      RETURN
+      RETURN
+      END SUBROUTINE
