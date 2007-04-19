a50f434ba621c11d6f6788b2ae430702d5190b2d
m_utkej <m_utkej@anlext2wls142>
2007-04-19 10:51:40 -0500

    more error handling

--- tools/SourceProcessing/postProcess.py
+++ tools/SourceProcessing/postProcess.py
@@ -16,6 +16,10 @@ from PP.xaifpp import val_deriv_m as valm
 from PP.xaifpp import type_active_m as actm
 from PP.xaifpp import add_active_module as addm
 
+from PyUtil.errors import UserError, ScanError, ParseError
+from PyUtil.assembler import AssemblerException as AsE
+from PyUtil.l_assembler import AssemblerException as LAsE
+
 def normal(self,arg):
     'attach self to prog repr'
     arg.attach(self)
@@ -30,7 +34,8 @@ def noop(self,*a):
     pass
 
 def main():
-  opt = OptionParser()
+  usage = '%prog [options] <input_file>'
+  opt = OptionParser(usage=usage)
   opt.add_option('-f','--forward',dest='fwd',
                  help="use forward mode postprocessing",
                  action='store_true',default=False)
@@ -45,19 +50,41 @@ def main():
                  help='inline definition file(default=ad_inline.f)',
                  metavar='FILE')
   config, args = opt.parse_args()
+  if len(args) != 1:
+     opt.error("expect input file argument")
   fn         = args[0]
-  (base,ext) = os.path.splitext(fn)
-  newfn      = base + '.pp' + ext
-  exp1  = mt.LexiMutator([(fe._Exp,noop),
-                          (fe.App,valm),
-                          ])
-  vstr  = lv.LexiVisitor([(fs.GenStmt,normal),
-                          (fs.Exec,exec_s),
-                          (fs.DrvdTypeDecl,actm),
-                          (fs.UseStmt,addm),
-                          ],'build')
-  xaifv = lv.MultiLexiVisitor(vstr,exp1)
-  Prog1(xaifv,fpf(fn).lines).writeit(newfn)
+  try: 
+    (base,ext) = os.path.splitext(fn)
+    newfn      = base + '.pp' + ext
+    exp1  = mt.LexiMutator([(fe._Exp,noop),
+                            (fe.App,valm),
+                            ])
+    vstr  = lv.LexiVisitor([(fs.GenStmt,normal),
+                            (fs.Exec,exec_s),
+                            (fs.DrvdTypeDecl,actm),
+                            (fs.UseStmt,addm),
+                            ],'build')
+    xaifv = lv.MultiLexiVisitor(vstr,exp1)
+    Prog1(xaifv,fpf(fn).lines).writeit(newfn)
+  except UserError,e : 
+    print >>sys.stderr, "Error: ", e.msg
+    return 1 
+  except ScanError,e : 
+    print >>sys.stderr, "Error: scanner fails at line:", e.aFortLine.line
+    print >>sys.stderr, " tokens scanned ok: ", e.scanned
+    print >>sys.stderr, "    unable to scan: ", e.rest
+    print >>sys.stderr, " This failure is likely due to possibly legal but unconventional Fortran,"
+    print >>sys.stderr, " such as unusual spacing. Please consider modifying your source code."
+    return 1 
+  except ParseError,e : 
+    print >>sys.stderr, "Error: parser fails to assemble tokens in scanned line:", e.scannedLine, " as ", e.target
+    return 1 
+  except AsE, e: 
+    print >>sys.stderr, "Internal Error: parser failed:", e.msg
+    return 1 
+  except LAsE, e: 
+    print >>sys.stderr, "Internal Error: parser failed:", e.msg
+    return 1 
   return 0
 
 if __name__ == "__main__":
