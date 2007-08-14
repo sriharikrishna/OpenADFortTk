f7180e465d82fb3c8287412395a17a10216b11f5
m_utkej <m_utkej@utke2.mcs.anl.gov>
2007-08-14 11:23:44 -0500

    cleanup and add PSTORE to basetype

--- src/lib/support/Open64IRInterface/wn_attr.h
+++ src/lib/support/Open64IRInterface/wn_attr.h
@@ -7,61 +7,11 @@
 #include "Open64BasicTypes.h"
 
 
-  /**
-   * Access-macros and access-functions for obtaining attributes of 
-   * WN trees for general use in translating WN to another language
-   * form (e.g. to C or Fortran).  For the most part, this module
-   * supplements common/com/wn_core.h.
- *
- *
- * Macros yielding OPCODE information:
- * -----------------------------------
- *
- *   INTR_is_adrtmp:
- *      An ADRTMP intrinsic opcode.
- *
- *   INTR_is_valtmp:
- *      An VALTMP intrinsic opcode.
- *
- * Functions yielding WN subtree information
- * ----------------------------------------
- *
- * WN_intrinsic_name:
- *      The name of the function or macro representing the intrinsic.
- *      Special handling is needed when the name is NULL.
- *
- * WN_intrinsic_return_ty:
- *      Map the intrinsic code to a return type.  If no return type
- *      is known, then use the result type of the OPCODE as the
- *      return type.
- *
- * WN_intrinsic_return_to_param:
- *      TRUE when the given return type is returned through the first
- *      parameter; FALSE when returning through pregs.
- *
- * Functions yielding WN subtree information
- * ----------------------------------------
- *
- *   WN_Get_PtrAdd_Intconst:
- *      Given the two operands of an ADD expression, see if this
- *      can be considered a pointer addition, and if it is return
- *      a pointer to an INTCONST that can be normalized w.r.t. the
- *      size of the pointed_ty; if the pointed_ty has size 1, then
- *      just return the integral expression operand when this should
- *      be considered a pointer addition; otherwise return NULL when
- *      this should not be considered a pointer addition.
- *
- *   WN_Tree_Type:
- *      The TY denoting the type of a WN expression.  For typeless
- *      WNs (e.g. statements), the TY will be "void" in C terminology.
- *      Similarly, there may be expression we cannot sensibly type,
- *      e.g. for MLOADs, in which case we return an inaccurate type.
- *      For MLOADs we return the type of a field at the given offset
- *      and of the given size, provided the size is known statically;
- *      otherwise we return the base-type.
- *
- * ====================================================================
- * ====================================================================
+/**
+ * Access-macros and access-functions for obtaining attributes of 
+ * WN trees for general use in translating WN to another language
+ * form (e.g. to C or Fortran).  For the most part, this module
+ * supplements common/com/wn_core.h.
  */
 
 /* several craylib/dope items represent a no-op by a zero inconst...*/
@@ -80,9 +30,9 @@
    ((WN_IOITEM(item) >= IOL_ARRAY && WN_IOITEM(item) <= IOL_VAR) || \
     (WN_IOITEM(item) == IOL_DOPE))
 
-//***************************************************************************
-
-
+/**
+ *  an ADRTMP intrinsic opcode.
+ */
 #define INTR_is_adrtmp(intrn) \
    ((intrn) == INTRN_U4I1ADRTMP || \
     (intrn) == INTRN_U4I2ADRTMP || \
@@ -107,6 +57,9 @@
     (intrn) == INTRN_U8CQADRTMP || \
     (intrn) == INTRN_U8VADRTMP)
 
+/**
+ * a VALTMP intrinsic opcode
+ */
 #define INTR_is_valtmp(intrn) \
    ((intrn) == INTRN_I4VALTMP || \
     (intrn) == INTRN_I8VALTMP || \
@@ -121,29 +74,46 @@
    ((arg)!=NULL && WN_operator(arg) == OPR_PARM? WN_kid0(arg) : arg)
 
 
-//***************************************************************************
-
+/**
+ * The TY_IDX denoting the type of a WN expression.  For typeless
+ * WNs (e.g. statements), the TY will be "void" in C terminology.
+ * Similarly, there may be expression we cannot sensibly type,
+ * e.g. for MLOADs, in which case we return an inaccurate type.
+ * For MLOADs we return the type of a field at the given offset
+ * and of the given size, provided the size is known statically;
+ * otherwise we return the base-type.
+ * Returns a TY_IDX that as closely as possible represents the type of the
+ * given subexpression (wn).  Supports calls (both as statements and
+ * expressions).  Also supports stores, returning the type of the
+ * referenced object (possibly dereferencing a pointer type).
+ * Note: TY_is_logical() will only hold true when the TY is resolved
+ * from a WN_ty or ST_ty attribute, not when it is resolved from an
+ * MTYPE.
+ * Note: Pointer types may be created as a result of a call to this
+ * routine.
+ */
 extern TY_IDX 
 WN_Tree_Type(const WN *wn);
 
 extern TY_IDX 
 Get_Field_Type(TY_IDX base, int field_id);
 
-//***************************************************************************
-
-// WN_GetRefObjType, WN_GetBaseObjType: For loads/stores, these are
-// types for 1) the base object and 2) the referenced object.  The
-// referenced object's address is at some offset (possibly 0) from the
-// base object.  Loads refer the rvalue object; stores refer to the
-// lvalue object.
+/** 
+ * For loads/stores: the ref object type is the type 
+ * after all 'dereferencing' has been done,
+ * e.g. for t%f it should be the type of the 
+ * field f in type t
+ */ 
 extern TY_IDX 
 WN_GetRefObjType(const WN* wn);
 
+/**
+ * For loads/stores: the base object type is the type
+ * before any 'dereferencing' has been done,
+ * e.g. for t%f it should be the type t
+ */
 extern TY_IDX 
 WN_GetBaseObjType(const WN* wn);
-
-
-// Calls and intrinsic calls
 
 extern TY_IDX 
 WN_Call_Type(const WN* wn);
@@ -157,20 +127,38 @@ WN_Call_First_Arg_Idx(const WN* wn);
 extern INT
 WN_Call_Last_Arg_Idx(const WN* wn);
 
-
+/**
+ * The name of the function or macro representing the intrinsic.
+ * Special handling is needed when the name is NULL.
+ */
 extern const char * 
 WN_intrinsic_name(INTRINSIC intr_opc);
 
+/**
+ * Map the intrinsic code to a return type.  If no return type
+ * is known, then use the result type of the OPCODE as the
+ * return type.
+ */
 extern TY_IDX 
 WN_intrinsic_return_ty(const WN *call);
 
+/**
+ * TRUE when the given return type is returned through the first
+ * parameter; FALSE when returning through pregs.
+ */
 extern BOOL 
 WN_intrinsic_return_to_param(TY_IDX return_ty);
 
-
+/**
+ * Given the two operands of an ADD expression, see if this
+ * can be considered a pointer addition, and if it is return
+ * a pointer to an INTCONST that can be normalized w.r.t. the
+ * size of the pointed_ty; if the pointed_ty has size 1, then
+ * just return the integral expression operand when this should
+ * be considered a pointer addition; otherwise return NULL when
+ * this should not be considered a pointer addition.
+ */
 extern WN *
 WN_Get_PtrAdd_Intconst(WN* wn0, WN* wn1, TY_IDX pointed_ty);
-
-//***************************************************************************
 
 #endif 
