ffe3c921e8acc7c30daaaeed68712d3243d7bf82
lyonsam <lyonsam@cuban>
2008-11-05 16:36:17 -0600

    removed some obsolescent canonicalizer stuff

--- tools/SourceProcessing/PyFort/UnitTests/Tfiles/t0.f
+++
@@ -1,9 +0,0 @@
-      subroutine foo(x,y,z)
-          type(OpenAD_type) x,xx1
-          real y,z
-          use w2f__types
-
-          y = z * z
-          __value__(x) = z * __value__(x)
-          call goo(y,__deriv__(x),z)
-      end
--- tools/SourceProcessing/PyIR/UnitTests/Tfiles/f.tmp.f
+++
@@ -1,7 +0,0 @@
-c Simple Test File
-c
-       function tmp(x,y)
-       integer x
-       real y
-       y = sin(x)*x
-       end
